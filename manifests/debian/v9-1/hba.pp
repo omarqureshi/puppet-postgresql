ae3c5cd5a4bf13d31ffa1275175fe6d53dce69ac
Omar Qureshi <omaraliqureshi@googlemail.com>
2012-06-11 12:05:41 +0100

    Aim to get rid of the Augeas dependancy

---
+++ manifests/debian/v9-1/hba.pp
@@ -0,0 +1,14 @@
+class postgresql::debian::v9-1::hba($clients) {
+  $version = "9.1"
+  if $clients = "" {
+    $pg_clients = []
+  } else {
+    $pg_clients = $clients
+  }
+
+  file {"/etc/postgresql/9.1/main/pg_hba.conf":
+    content => template("postgresql/hba.erb"),
+    require => Package["postgresql-${version}"],
+    notify => Exec["reload postgresql ${version}"],
+  }
+}
